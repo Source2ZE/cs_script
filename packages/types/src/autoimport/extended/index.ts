e57c251ca31bd83cc177916bb1a75f02a0b6b29d
Poggu <poggu@seznam.cz>
2025-09-24 20:30:35 +0200

    feat(types): add more generic options

--- packages/types/src/autoimport/extended/index.ts
+++ packages/types/src/autoimport/extended/index.ts
@@ -9,9 +9,14 @@ declare module 'cs_script/point_script' {
     FindEntityByClass<T extends keyof EntityClassMap>(
       className: T
     ): EntityClassMap[T] | undefined
-
     FindEntitiesByClass<T extends keyof EntityClassMap>(
       className: T
     ): EntityClassMap[T][] | undefined
+
+    FindEntityByClass<T extends Entity>(className: string): T | undefined
+    FindEntitiesByClass<T extends Entity>(className: string): T[] | undefined
+
+    FindEntityByName<T extends Entity>(className: string): T | undefined
+    FindEntitiesByName<T extends Entity>(className: string): T[] | undefined
   }
 }
